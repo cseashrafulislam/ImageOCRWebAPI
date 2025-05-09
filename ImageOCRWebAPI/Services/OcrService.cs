b4d2f1e3bdfacb314a7debc9d051e45dd8b4352b
Md. Ashraful Islam <Mimashraful@outlook.com>
2025-05-09 23:16:43 +0600

    --

--- ImageOCRWebAPI/Services/OcrService.cs
+++ ImageOCRWebAPI/Services/OcrService.cs
@@ -4,15 +4,17 @@ namespace ImageOCRWebAPI.Services
 {
     public class OcrService
     {
+        //Download Dataset : https://github.com/tesseract-ocr/tessdata
         public string ExtractTextFromImage(string imagePath)
         {
             var tessDataPath = Path.Combine(Directory.GetCurrentDirectory(), "Tesseract");
 
-            using var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default);
+            using var engine = new TesseractEngine(tessDataPath, "ben+eng", EngineMode.Default);
             using var img = Pix.LoadFromFile(imagePath);
             using var page = engine.Process(img);
             return page.GetText();
         }
+
 
     }
 }
